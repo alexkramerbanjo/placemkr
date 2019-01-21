b549e7d8546fbde1a16d323576883ea3989d6982
Alex Kramer <alexkrameris@gmail.com>
2019-01-20 22:19:43 -0500

    can now write to database

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Form, Button } from "native-base";
-import { View } from "expo-graphics";
+import { Form, Button, Label } from "native-base";
 import { TextInput } from "react-native-gesture-handler";
 import { database } from "../firebase/Fire";
+import { View, Text } from "react-native";
 
 function showDistance(lat1, long1, lat2, long2) {
   var distance = distanceInKmBetweenEarthCoordinates(lat1, long1, lat2, long2);
@@ -31,21 +31,25 @@ export default class SettingsScreen extends React.Component {
       location: { latitude: null, longitude: null },
       error: null
     };
+    this.sendForm = this.sendForm.bind(this);
   }
   static navigationOptions = {
     title: "app.json"
   };
 
   sendForm(formData) {
+    console.log(this.state);
     const data = {
       location: this.state.location,
       title: this.state.title,
       author: this.state.author,
       message: this.state.message
     };
+    console.log(data);
     const lat = Number.parseFloat(data.location.latitude).toFixed(4);
     const long = Number.parseFloat(data.location.longitude).toFixed(4);
-    database.ref(`${lat}_${long}/texts/${this.state.title}`).set();
+    const loc = `madePlaces/${lat}/${long}`.replace(/\./g, "_");
+    database.ref(`${loc}/texts/${this.state.title}`).set(data);
   }
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(
@@ -65,24 +69,33 @@ export default class SettingsScreen extends React.Component {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
     return (
-      <View style={styles.container}>
+      <View>
         <Text>
-          Latitude: {this.state.latitude}
+          Latitude: {this.state.location.latitude}
           {"\n"}
-          Longitude: {this.state.longitude}
+          Longitude: {this.state.location.longitude}
           {this.state.error ? this.state.error : ""}
         </Text>
         <View>
+          <Label>Message: </Label>
           <TextInput
+            style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+            title="message"
             onChangeText={stuff => this.setState({ message: stuff })}
             value={this.state.message}
           />
+          <Label>Name: </Label>
           <TextInput
+            style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+            title="author"
             onChangeText={name => this.setState({ author: name })}
             value={this.state.author}
           />
+          <Label>Title: </Label>
           <TextInput
-            onChangeText={title => this.setState({ title: name })}
+            style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+            title="title"
+            onChangeText={title => this.setState({ title: title })}
             value={this.state.title}
           />
         </View>
@@ -91,7 +104,9 @@ export default class SettingsScreen extends React.Component {
           onPress={this.sendForm}
           title="Send"
           style={{ width: 100, paddingTop: 20 }}
-        />
+        >
+          <Text>SEND!</Text>
+        </Button>
       </View>
     );
   }
